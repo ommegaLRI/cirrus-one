135ff290c19944caf07db19b8d5b951742800f59
Marco Rodriguez <marcosaysrelax@gmail.com>
2026-03-13 12:53:01 -0400

    Update app.py

--- deid/api/app.py
+++ deid/api/app.py
@@ -26,7 +26,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",  # Next.js dev server
-        "http://cirrus-dash.onrender.com",
+        "https://cirrus-dash.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
